011b26f2c94ac20baad2b43f2346ada6c31badfe
ravichas <saka.ravi@gmail.com>
2020-11-06 17:59:47 -0500

    tc1_library.py (a Python file for plotting PCA/t-SNE results)

--- tc1_library.py
+++ tc1_library.py
@@ -1,7 +1,10 @@
 # This is based on code here: https://github.com/andrew-weisman/target_classification
-# Feel free to contact (Andrew) any questions about this
+# Feel free to ask me (Andrew) any questions about this
 
-def plot_results(results_to_plot, MYLABELS):
+def plot_unsupervised_learning_results(unsupervised_learning_results, labels):
+	'''
+	Plot results from unsupervised learning analyses such as PCA or tSNE, overplotting the color/style of the markers based on known labels.
+	'''
 
 	# Import relevant libraries
 	import seaborn as sns
@@ -10,13 +13,38 @@ def plot_results(results_to_plot, MYLABELS):
 
 	# Get a reasonable set of markers and color palette
 	markers = mpl_lines.Line2D.filled_markers
-	nclasses = len(set(MYLABELS))
+	nclasses = len(set(labels))
 	marker_list = (markers * int(nclasses/len(markers)+1))[:nclasses]
 	color_palette = sns.color_palette("hls", nclasses)
 
 	# Plot results
 	_, ax = plt.subplots(figsize=(12,7.5), facecolor='w')
-	ax = sns.scatterplot(x=results_to_plot[:,0], y=results_to_plot[:,1], hue=MYLABELS, style=MYLABELS, palette=color_palette, legend='full', alpha=1, markers=marker_list, edgecolor='k', ax=ax)
+	ax = sns.scatterplot(x=unsupervised_learning_results[:,0], y=unsupervised_learning_results[:,1], hue=labels, style=labels, palette=color_palette, legend='full', alpha=1, markers=marker_list, edgecolor='k', ax=ax)
 	ax.legend(bbox_to_anchor=(1,1))
 
 	return(ax)
+
+
+def run_and_plot_pca_and_tsne(X, y, top_n_features=500):
+	'''
+	Run PCA and tSNE analyses on data X and plot the results with marker color/style by known labels y.
+	Note that tSNE takes long if using many features, so for tSNE we use the top-top_n_features-variance genes.
+	'''
+
+	# Sort the features by decreasing variance
+	X_by_variance = X.iloc[:,[int(x) for x in X.var().sort_values(ascending=False).index]]
+
+	# Perform the PCA using scikit-learn
+	import sklearn.decomposition as sk_decomp
+	pca = sk_decomp.PCA(n_components=10)
+	pca_results = pca.fit_transform(X_by_variance)
+	print('Top {} PCA explained variance ratios: {}'.format(10, pca.explained_variance_ratio_))
+	ax = plot_unsupervised_learning_results(pca_results, y)
+	ax.set_title('PCA')
+
+	# Perform tSNE using scikit-learn
+	import sklearn.manifold as sk_manif
+	tsne = sk_manif.TSNE(n_components=2)
+	tsne_results = tsne.fit_transform(X_by_variance.iloc[:,:top_n_features])
+	ax = plot_unsupervised_learning_results(tsne_results, y)
+	ax.set_title('tSNE (using top-{}-variance features)'.format(top_n_features))
